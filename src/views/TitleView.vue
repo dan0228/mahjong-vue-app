010c7410e825b073609502afec078854dd34f937
dan <ryu.dan.stu@gmail.com>
2025-12-22 01:14:56 +0900

    v1.7.2

--- src/views/TitleView.vue
+++ src/views/TitleView.vue
@@ -99,7 +99,7 @@
         <div class="x-account">
           <a href="https://x.com/danAllGreen" target="_blank" rel="noopener noreferrer">{{ $t('titleView.officialX') }}</a>
         </div>
-        <div class="version-info">v1.7.1 | 2025.10.19</div>
+        <div class="version-info">v1.7.2 | 2025.12.22</div>
       </footer>
 
       <!-- ポップアップ -->
